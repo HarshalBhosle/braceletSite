1b2084b071dab2ca08e23cafb0bddb44f1f02d48
HarshalBhosle <harsalb1202@gmail.com>
2026-05-21 00:01:21 +0530

    corrected mistakes

--- bracelet_project/urls.py
+++ bracelet_project/urls.py
@@ -18,11 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from bracelets.views import landing_page
+from bracelets.views import bracelets_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', landing_page, name='landing'),
+    path('', bracelets_list, name='home'),
     path('bracelets/', include('bracelets.urls')),
 ]
 
