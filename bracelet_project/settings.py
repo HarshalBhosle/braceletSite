7151fb678bb9cab54916317edcc1678c820d1b94
Amol C <amolch001@gmail.com>
2026-05-22 00:54:52 +0530

    added proper stylesheet and theme

--- bracelet_project/settings.py
+++ bracelet_project/settings.py
@@ -198,11 +198,11 @@ LOGOUT_REDIRECT_URL = "/"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"] if (BASE_DIR / "static").exists() else []
 
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 
